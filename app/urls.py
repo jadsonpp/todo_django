182827f3a41537f72229502dbebdb0be8a03cc57
jadson <jadson_pereira5@hotmail.com>
2020-05-11 23:11:22 -0300

    Adjust initial page

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,9 @@ from .views.task import *
 from .views.user import *
 
 urlpatterns = [
+    #Login and logout
+    path("",login_user,name="login"),
+    path("logout",logout_user,name="logout"),
     #CRUD Task
     path("tasks/",list_tasks, name= "tasks"),
     path("task_register",create_task,name="register"),
@@ -11,7 +14,4 @@ urlpatterns = [
     path("delete/<int:id>",destroyTask,name="delete"),
     #Register User
     path("user_register", register_user,name ='user_register'),
-    #Login and logout
-    path("login",login_user,name="login"),
-    path("logout",logout_user,name="logout"),
 ]
